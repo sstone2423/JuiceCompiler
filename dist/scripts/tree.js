ae469532aad31c35fb2d698b9bb00f39f51ab161
Stephen Stone <sstone2423@gmail.com>
2019-02-26 12:58:39 -0500

    Initial Tree functions added. Untested at the moment

--- dist/scripts/tree.js
+++ dist/scripts/tree.js
@@ -2,12 +2,189 @@
 /*
     tree.ts
 */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var JuiceC;
 (function (JuiceC) {
     var Tree = /** @class */ (function () {
         function Tree() {
+            this.curr = null;
+            this.root = null;
         }
+        // Adds non-terminal node to tree
+        Tree.prototype.addNTNode = function (production, lineNumber, colNumber) {
+            var node = new NonTerminalTreeNode(production);
+            node.lineNumber = lineNumber;
+            node.colNumber = colNumber;
+            if (this.root == null) {
+                this.root = node;
+                this.curr = node;
+                return;
+            }
+            // Set parent node
+            node.parent = this.curr;
+            // Add to children of curr node
+            this.curr.children.push(node);
+            this.descendTree();
+        };
+        // Adds terminal node to tree AKA leaf node
+        Tree.prototype.addTNode = function (token) {
+            var node = new TerminalTreeNode(token);
+            if (this.root == null) {
+                this.root = node;
+                this.curr = node;
+                return;
+            }
+            // Set parent node
+            node.parent = this.curr;
+            // Add to children of curr node
+            this.curr.children.push(node);
+        };
+        // Add general node
+        Tree.prototype.addNode = function (input) {
+            var node = new GeneralTreeNode(input);
+            if (this.root == null) {
+                this.root = node;
+                this.curr = node;
+                return;
+            }
+            // Set parent node
+            node.parent = this.curr;
+            // Add to children of curr node
+            this.curr.children.push(node);
+            this.descendTree();
+        };
+        // Sets the current node as the latest child
+        Tree.prototype.descendTree = function () {
+            if (this.curr == null) {
+                return;
+            }
+            var latestChild = this.curr.children[this.curr.children.length - 1];
+            this.curr = latestChild;
+        };
+        // Sets the current node as the parent of the current node
+        Tree.prototype.ascendTree = function () {
+            this.curr = this.curr.parent;
+        };
+        // Prints the tree in depth-first search order for CST display
+        Tree.prototype.traverseTreeCST = function (treantTree, programCounter) {
+            var tree = [];
+            var level = 0;
+            if (this.root != null) {
+                this.DFSCST(this.root, level, tree, "", treantTree['nodeStructure'].children, programCounter);
+            }
+            // Return array of nodes and tree config
+            return { "tree": tree, "treant": treantTree };
+        };
+        // Returns an array representation of depth-first search of tree
+        Tree.prototype.traverseTree = function () {
+            var tree = [];
+            if (this.root != null) {
+                this.DFSTree(this.root, tree);
+            }
+            return tree;
+        };
+        // Recursively push nodes into the traverseTree() tree
+        Tree.prototype.DFSTree = function (node, tree) {
+            tree.push(node);
+            for (var i = 0; i < node.children.length; i++) {
+                this.DFSTree(node.children[i], tree);
+            }
+        };
+        /**
+         * Helper for traverseTreeCST
+         */
+        Tree.prototype.DFSCST = function (node, level, tree, dash, treantTree, programCounter) {
+            var child = {};
+            if (node.value instanceof JuiceC.Token) {
+                tree.push(dash + "[" + node.value.value + "]");
+                // Add new node to children array passed
+                // Pass reference to new children array to next call
+                child = {
+                    text: { name: "[" + node.value.value + "]" },
+                    children: []
+                };
+                treantTree.push(child);
+            }
+            else {
+                var nodeValue = node.value;
+                // if node value is Program, put what program number it is
+                if (nodeValue == "Program") {
+                    nodeValue = nodeValue + "" + programCounter;
+                }
+                tree.push(dash + "<" + nodeValue + ">");
+                // Add new node to children array passed
+                // Pass reference to new children array to next call
+                child = {
+                    text: { name: "<" + nodeValue + ">" },
+                    children: []
+                };
+                treantTree.push(child);
+            }
+            for (var i = 0; i < node.children.length; i++) {
+                // to next call of DFS, increase level, pass the tree array, increase the dash by one dash, and pass
+                // the reference to the next children array
+                this.DFSCST(node.children[i], level + 1, tree, dash + "-", child['children'], programCounter);
+            }
+        };
         return Tree;
     }());
     JuiceC.Tree = Tree;
+    // Implementation of a TreeNode that makes up a Tree
+    var TreeNode = /** @class */ (function () {
+        function TreeNode(value) {
+            // the children this node points to
+            this.children = [];
+            this.value = value;
+        }
+        return TreeNode;
+    }());
+    JuiceC.TreeNode = TreeNode;
+    // A TreeNode that represents NonTerminals
+    var NonTerminalTreeNode = /** @class */ (function (_super) {
+        __extends(NonTerminalTreeNode, _super);
+        function NonTerminalTreeNode() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        NonTerminalTreeNode.prototype["super"] = function (value) {
+            this.value = value;
+        };
+        return NonTerminalTreeNode;
+    }(TreeNode));
+    JuiceC.NonTerminalTreeNode = NonTerminalTreeNode;
+    // A TreeNode that represents Terminals
+    var TerminalTreeNode = /** @class */ (function (_super) {
+        __extends(TerminalTreeNode, _super);
+        function TerminalTreeNode() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        TerminalTreeNode.prototype["super"] = function (value) {
+            this.value = value;
+        };
+        return TerminalTreeNode;
+    }(TreeNode));
+    JuiceC.TerminalTreeNode = TerminalTreeNode;
+    // A TreeNode that represents any value
+    var GeneralTreeNode = /** @class */ (function (_super) {
+        __extends(GeneralTreeNode, _super);
+        function GeneralTreeNode() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        GeneralTreeNode.prototype["super"] = function (value) {
+            this.value = value;
+        };
+        return GeneralTreeNode;
+    }(TreeNode));
+    JuiceC.GeneralTreeNode = GeneralTreeNode;
 })(JuiceC || (JuiceC = {}));
--- source/scripts/tree.ts
+++ source/scripts/tree.ts
@@ -5,14 +5,178 @@
 
 module JuiceC {
 
-	export class Tree {
+        export class Tree {
+            // Root of the tree
+            root: TreeNode;
+            // Current parent node we are looking at
+            curr: TreeNode;
+    
+            constructor() {
+                this.curr = null;
+                this.root = null;
+            }
 
-        
-        
-        constructor () {
+            // Adds non-terminal node to tree
+            public addNTNode(production: Production, lineNumber: number, colNumber: number) {
+                let node = new NonTerminalTreeNode(production);
+                node.lineNumber = lineNumber;
+                node.colNumber = colNumber;
+                if (this.root == null) {
+                    this.root = node;
+                    this.curr = node;
+                    return;
+                }
+                // Set parent node
+                node.parent = this.curr;
+                // Add to children of curr node
+                this.curr.children.push(node);
+                this.descendTree();
+            }
 
+            // Adds terminal node to tree AKA leaf node
+            public addTNode(token: Token) {
+                let node = new TerminalTreeNode(token);
+                if (this.root == null) {
+                    this.root = node;
+                    this.curr = node;
+                    return;
+                }
+                // Set parent node
+                node.parent = this.curr;
+                // Add to children of curr node
+                this.curr.children.push(node);
+            }
+
+            // Add general node
+            public addNode(input: any) {
+                let node = new GeneralTreeNode(input);
+                if (this.root == null) {
+                    this.root = node;
+                    this.curr = node;
+                    return;
+                }
+                // Set parent node
+                node.parent = this.curr;
+                // Add to children of curr node
+                this.curr.children.push(node);
+                this.descendTree();
+            }
+
+            // Sets the current node as the latest child
+            public descendTree() {
+                if (this.curr == null) {
+                    return;
+                }
+                let latestChild = this.curr.children[this.curr.children.length - 1];
+                this.curr = latestChild;
+            }
+
+            // Sets the current node as the parent of the current node
+            public ascendTree() {
+                this.curr = this.curr.parent;
+            }
+
+            // Prints the tree in depth-first search order for CST display
+            public traverseTreeCST(treantTree, programCounter) {
+                let tree: Array<String> = [];
+                let level: number = 0;
+                if (this.root != null) {
+                    this.DFSCST(this.root, level, tree, "", treantTree['nodeStructure'].children, programCounter);
+                }
+                // Return array of nodes and tree config
+                return {"tree": tree, "treant": treantTree};
+            }
+
+            // Returns an array representation of depth-first search of tree
+            public traverseTree() {
+                let tree: Array<TreeNode> = [];
+                if (this.root != null) {
+                    this.DFSTree(this.root, tree);
+                }
+                return tree;
+            }
+
+            // Recursively push nodes into the traverseTree() tree
+            public DFSTree(node, tree) {
+                tree.push(node);
+                for (let i = 0; i < node.children.length; i++) {
+                    this.DFSTree(node.children[i], tree);
+                }
+            }
+            
+            /**
+             * Helper for traverseTreeCST
+             */
+            private DFSCST(node, level, tree, dash, treantTree, programCounter) {
+                let child = {};
+                if (node.value instanceof Token) {
+                    tree.push(dash + "[" + node.value.value + "]")
+                    // Add new node to children array passed
+                    // Pass reference to new children array to next call
+                    child = {
+                        text: { name: "[" + node.value.value + "]" },
+                        children: []
+                    }
+                    treantTree.push(child);
+                } else {
+                    let nodeValue = node.value;
+                    // if node value is Program, put what program number it is
+                    if (nodeValue == "Program") {
+                        nodeValue = nodeValue + "" + programCounter;
+                    }
+                    tree.push(dash + "<" + nodeValue + ">")
+                    // Add new node to children array passed
+                    // Pass reference to new children array to next call
+                    child = {
+                        text: { name: "<" + nodeValue + ">" },
+                        children: []
+                    }
+                    treantTree.push(child);
+                }
+                for (let i = 0; i < node.children.length; i++) {
+                    // to next call of DFS, increase level, pass the tree array, increase the dash by one dash, and pass
+                    // the reference to the next children array
+                    this.DFSCST(node.children[i], level + 1, tree, dash + "-", child['children'], programCounter);
+                }
+            }
+        }
+    
+        // Implementation of a TreeNode that makes up a Tree
+        export abstract class TreeNode {
+            value: any;
+            // pointer to parent of this node
+            parent: TreeNode;
+            // the children this node points to
+            children = [];
+            constructor(value: any){
+                this.value = value;
+            }
+        }
+    
+        // A TreeNode that represents NonTerminals
+        export class NonTerminalTreeNode extends TreeNode {
+            value: Production;
+            lineNumber: number;
+            colNumber: number;
+            super(value: Production) {
+                this.value = value;
+            }
+        }
+    
+        // A TreeNode that represents Terminals
+        export class TerminalTreeNode extends TreeNode {
+            value: Token;
+            super(value: Token) {
+                this.value = value;
+            }
         }
 
-    }
+        // A TreeNode that represents any value
+        export class GeneralTreeNode extends TreeNode {
+            value: any;
+            super(value: any){
+                this.value = value;
+            }
+}
 
 }
